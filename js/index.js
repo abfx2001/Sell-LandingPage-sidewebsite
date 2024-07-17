4d69a01f5e88522c303e73d0f9a4e0d6389083a7
Arthur Batista Furlan <arthurbfx2001@gmail.com>
2024-07-17 14:41:13 -0300

    v.0.1.2

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   var navbar = document.getElementById('navbar');
+  var logoImg = document.getElementById('logo-img');
 
   window.onscroll = function() {
     if (window.scrollY > 100) { // ajuste o valor conforme necessário
       navbar.classList.add('scrolled');
       navbar.classList.add('fixed-top');
+      logoImg.src = '/img/icons/Logo-Sell-Adm-Colorido.png';
     } else {
       navbar.classList.remove('scrolled');
       navbar.classList.remove('fixed-top');
+      logoImg.src = '/img/icons/Logo-Sell-Adm-Branco.png';
     }
   };
 });
