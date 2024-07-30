3df23fefde7366947f7a3aab63bb0565afa5d14b
Arthur Batista Furlan <arthurbfx2001@gmail.com>
2024-07-30 16:43:23 -0300

    v.0.3.2

--- js/index.js
+++ js/index.js
@@ -6,16 +6,97 @@ document.addEventListener("DOMContentLoaded", function() {
     if (window.scrollY > 100) {
       navbar.classList.add('scrolled');
       navbar.classList.add('fixed-top');
+      logoImg.classList.add('img-scrolled')
       logoImg.src = '/img/icons/Logo-Sell-Adm-Colorido.png';
     } else {
       navbar.classList.remove('scrolled');
       navbar.classList.remove('fixed-top');
+      logoImg.classList.remove('img-scrolled')
       logoImg.src = '/img/icons/Logo-Sell-Adm-Branco.png';
     }
   };
 });
 
-
 //constructor
 window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-txt1', {
+  duration: 1000,
+  distance: '100px'
+})
+
+revelar.reveal('.efeito-btn1', {
+  duration: 1000,
+  distance: '1000px',
+  delay: 700,
+  origin: 'left'
+})
+
+revelar.reveal('.efeito-btn2', {
+  duration: 1000,
+  distance: '1000px',
+  delay: 400,
+  origin: 'left'
+})
+
+revelar.reveal('.efeito-txt2', {
+  duration: 3000,
+  distance: '100px'
+})
+
+revelar.reveal('.efeito-txt2-1', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 200
+})
+
+revelar.reveal('.efeito-txt2-2', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 300
+})
+
+revelar.reveal('.efeito-txt2-3', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 400
+})
+
+revelar.reveal('.efeito-txt2-4', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 500
+})
+
+revelar.reveal('.efeito-txt2-5', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 600
+})
+
+revelar.reveal('.efeito-txt3-1', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 100
+})
+
+revelar.reveal('.efeito-txt3-2', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 300
+})
+
+revelar.reveal('.efeito-txt3-3', {
+  duration: 2000,
+  distance: '100px',
+  origin: 'right',
+  delay: 400
+})
 
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 # Landing Page - Solicitar Orçamento e Prestador de Serviço
 
 > version 0.1.4
+
 > version 0.2.0
+
+> version 0.3.0
